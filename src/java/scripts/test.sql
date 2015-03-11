c9d1783e565c5c3a0da04b92a769eebd220c661d
nikolai94 <nikolai@cybermanager.dk>
2015-03-11 11:43:57 +0100

    script og fix og web-inf tilføjet

--- src/java/scripts/test.sql
+++ src/java/scripts/test.sql
@@ -1,2 +1,15 @@
-INSERT INTO CITYINFO (ID,CITY,ZIPCODE) VALUES (1,'Scanning','0555')
+
+
+INSERT INTO ADDRESS (id,AdditionalInfo,street,CITYINFO_ZIPCODE)
+VALUES (1,'1tv','Nørrebrogade','2200');
+INSERT INTO ADDRESS (id,AdditionalInfo,street,CITYINFO_ZIPCODE)
+VALUES (2,'2tv','Vesterbrogade','1600');
+INSERT INTO ADDRESS (id,AdditionalInfo,street,CITYINFO_ZIPCODE)
+VALUES (3,'3tv','Østerbrogade','2100');
+INSERT INTO ADDRESS (id,AdditionalInfo,street,CITYINFO_ZIPCODE)
+VALUES (4,'4tv','Amagerbrogade','2300');
+
+
+INSERT INTO InfoEntity(ID,DTYPE,EMAIL,ADDRESS_ID,FIRSTNAME,LASTNAME)
+VALUES (1,'Person','PERSON1@1.DK',1,'bruger 1', 'bruger 1 efternavn');
 
