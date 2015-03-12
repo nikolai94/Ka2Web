350fe006f9ce45b0fd54fea82b29caad889982a8
Nikolai94 <nikolai@cybermanager.dk>
2015-03-12 12:42:12 +0100

    html + ajax

--- web/person.js
+++ web/person.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
     $("#form").submit(function(){
         x=$('form').serializeArray();
         $.each(x, function(i, field){
@@ -17,7 +17,131 @@ $(function(){
         });
         return false;
     });
+});*/
+
+
+$("document").ready(function (){ 
+ $("#submit_allOnZip").click(findOnZip);
+ $("#findAllPerson").click(findAllPerson);
+ $("#findAllZip").click(findAllZips);
+
 });
 
+function findAllZips(){
+    
+     var request = $.ajax({
+     url : "api/getzip",
+     dataType : "json",
+     type : "GET"
+     });
+     
+     request.done(function(Zip){
+            for (var i = 0; i < Zip.length; i++) {
+                //Zip[i].
+            }
+     }); 
+    
+    
+}
+            
+function findOnZip(){
+     $("#bodyGetPersons").text(" ");
+     var value = $( '#zipcode' ).val();
+     var request = $.ajax({
+     url : "api/getPersonOnZip/"+value,
+     dataType : "json",
+     type : "GET"
+     });
+     
+     
+         request.done(function(Person){
+         for (var i = 0; i < Person.length; i++) {
+          var insertText ="<tr>";
+          insertText += "<td>"+ Person[i].firstname +"</td>";
+          insertText += "<td>"+ Person[i].lastname +"</td>";
+          insertText += "<td>"+ Person[i].street +"</td>";
+          insertText += "<td>"+ Person[i].additionalinfo +"</td>";
+             insertText += "<td>";
+             for (var j = 0; j < Person[i].hobbies.length; j++) {
+                insertText += Person[i].hobbies[j].name+"<br/>";
+            }
+          insertText += "</td>";
+          insertText += "<td>"+ Person[i].email +"</td>";
+          
+          insertText += "<td>";
+            for (var k = 0; k < Person[i].Phones.length; k++) {
+                 insertText += Person[i].Phones[k].number+"<br/>";
+            }
+          insertText += "</td>";
+          
+          insertText += "<td>";
+            insertText += Person[i].zipcode ;
+          insertText += "</td>";
+          
+          insertText += "<td>";
+            insertText += Person[i].city;
+          insertText += "</td>";
+          
+             
+          insertText += "</tr>";
+          $("#bodyGetPersons").append(insertText);   
+        }   
+         
+      });            
 
+      request.fail(function( jqXHR, textStatus ) {
+            alert( "Request failed: " + textStatus );
+       });
+         
+}
+
+
+function findAllPerson (){ 
+    $("#bodyGetPersons").text(" ");
+     var request = $.ajax({
+     url : "api/complete",
+     dataType : "json",
+     type : "GET"
+     });
+     
+     request.done(function(Person){
+         for (var i = 0; i < Person.length; i++) {
+          var insertText ="<tr>";
+          insertText += "<td>"+ Person[i].firstname +"</td>";
+          insertText += "<td>"+ Person[i].lastname +"</td>";
+          insertText += "<td>"+ Person[i].street +"</td>";
+          insertText += "<td>"+ Person[i].additionalinfo +"</td>";
+             insertText += "<td>";
+             for (var j = 0; j < Person[i].hobbies.length; j++) {
+                insertText += Person[i].hobbies[j].name+"<br/>";
+            }
+          insertText += "</td>";
+          insertText += "<td>"+ Person[i].email +"</td>";
+          
+          insertText += "<td>";
+            for (var k = 0; k < Person[i].Phones.length; k++) {
+                 insertText += Person[i].Phones[k].number+"<br/>";
+            }
+          insertText += "</td>";
+          
+          insertText += "<td>";
+            insertText += Person[i].zipcode ;
+          insertText += "</td>";
+          
+          insertText += "<td>";
+            insertText += Person[i].city;
+          insertText += "</td>";
+          
+             
+          insertText += "</tr>";
+          $("#bodyGetPersons").append(insertText);   
+        }   
+         
+      });            
+
+      request.fail(function( jqXHR, textStatus ) {
+            alert( "Request failed: " + textStatus );
+       });
+
+}
 
